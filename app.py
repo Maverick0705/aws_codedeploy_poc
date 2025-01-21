70a7c383c3788c2b3a987309c21c518d381e02bb
Ritesh_Bhatt <112896716+Maverick0705@users.noreply.github.com>
2025-01-22 00:36:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, This is V2'
+     time.sleep(30)
+    return 'Hello, This is V3'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
