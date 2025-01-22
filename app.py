110f0bbc9061b67d80adb3beb981b0f98002d09c
Ritesh Bhatt <riteshbhatt777@gmail.com>
2025-01-22 15:06:11 +0530

    v12

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # time.sleep(30)  # Correct indentation and ensure time module is imported
-    return 'Hello, This is V11'
+    return 'Hello, This is V12'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
